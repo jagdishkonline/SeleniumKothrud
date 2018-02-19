f0366a83b5ef3735bedac15ee7b4ad6934a55023
jagdish <jagdishkonline@gmail.com>
2018-02-19 10:28:29 +0530

    POM FrameWork Data is Updated

--- POM_FrameworkN/SeleniumQTP/seleniumQTP_Tests/SeleniumQTP_RegistrationPage1_Test.java
+++ POM_FrameworkN/SeleniumQTP/seleniumQTP_Tests/SeleniumQTP_RegistrationPage1_Test.java
@@ -14,7 +14,7 @@ public class SeleniumQTP_RegistrationPage1_Test extends BaseClass {
 	//  driver.get("https://seleniumqtpautomation.000webhostapp.com/final.html");
 	  SeleniumQTP_RegistrationPage2_Verification regP2=  regPV.verifyRegPage1DataEntered("user", "pass", "confPass");//Imp Step we r returning here SeleniumQTP_RegistrationPage2_Verification
 	 Assert.assertEquals("Reg2", regP2.verifyTitle());//we are verifying page2 Title
-	 regP2.verifyEnterPage2Details("abc","xyz","abc@gmail.com","9762215699");
+	 regP2.verifyEnterPage2Details("Jagdish","Kamble","jagdishkonline@gmail.com","9762215699");
   
   
   }
